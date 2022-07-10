42e9630b046dae81ce85aa45d0e39e56d0df6fb8
Keith <redundently1234@gmail.com>
2022-07-09 22:42:35 -0400

    conversion of consolewindow struct to class.  lots of cleanup.

--- Main.cpp
+++ Main.cpp
@@ -4,13 +4,13 @@
 
 int main(int, char**)
 {
-    bool wait = true;
+    //bool wait = true;
     //Process valheimProc("valheim.x86_64", wait);
     //valheimProc.GetModuleBase("libmonobdwgc-2.0.so");
     Overlay overlay;
     if (overlay.isInitialized)
     {
-        overlay.Register((Overlay::UserOLFunc)MemPenguinOL);
+        overlay.Register((Overlay::UserOLFunc)ConsoleWindow::MemPenguinOL);
         overlay.Run();
     }
     return 0;
